6a80c4723e21e9e3e1064f00defec99dbc12a064
VladKapusta <vladkapysta96@gmail.com>
2023-02-18 16:47:51 +0200

    image Search

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Feedback } from 'components/FeedbackApp/Feedback';
-import { ContactBook } from 'components/ContactBook/ContactBook';
+
 import { ImageSearch } from 'components/ImageSearch/ImageSearch';
 
 import './index.css';
 
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Feedback />
-    <ContactBook />
     <ImageSearch />
   </React.StrictMode>
 );
